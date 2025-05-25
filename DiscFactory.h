f470a0e763d81a456bb2c87c10abc897a4490724
Daniel Scheeren <71975498+dscheeren622@users.noreply.github.com>
2025-05-25 14:29:18 +0200

    Add files via upload

---
+++ DiscFactory.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Position.h"
+#include "ColorEnum.h"
+
+class DiscFactory
+{
+
+    public:
+        DiscFactory() = default;
+        static void makeRedDisc(int columnNumber, Field &field);
+        static void makeYellowDisc(int columnNumber, Field &field);
+
+    private:
+};
